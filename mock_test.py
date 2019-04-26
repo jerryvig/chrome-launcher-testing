b70faf502bf33364642b86d27ce956332345e7ad
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-04-26 17:22:23 -0500

    updates

--- mock_test.py
+++ mock_test.py
@@ -1,12 +1,35 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.firefox import options
+
+DEFAULT_WAIT = 8
 
 profile = webdriver.FirefoxProfile('/home/jerry/.mozilla/firefox/huqgok2n.default')
-driver = webdriver.Firefox(profile)
+options = options.Options()
+options.headless = True
+driver = webdriver.Firefox(profile, options=options)
+driver.implicitly_wait(DEFAULT_WAIT)
 
 driver.get('https://www.linkedin.com/feed/')
+time.sleep(DEFAULT_WAIT + 5)
+driver.get_screenshot_as_file('linkedin-feed.png')
 
-driver.implicitly_wait(15)
 
-driver.get_screenshot_as_file('linkedin.png')
+driver.get('https://www.linkedin.com/mynetwork/')
+time.sleep(DEFAULT_WAIT)
+driver.get_screenshot_as_file('linkedin-mynetwork.png')
+
+driver.get('https://www.linkedin.com/jobs/')
+time.sleep(DEFAULT_WAIT)
+driver.get_screenshot_as_file('linkedin-jobs.png')
+
+driver.get('https://www.linkedin.com/jobs/')
+time.sleep(DEFAULT_WAIT)
+driver.get_screenshot_as_file('linkedin-jobs.png')
+
+driver.get('https://www.linkedin.com/notifications/')
+time.sleep(DEFAULT_WAIT)
+driver.get_screenshot_as_file('linkedin-notifications.png')
 
 driver.quit()
