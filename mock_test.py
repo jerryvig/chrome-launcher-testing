c6e403dddb70e423e9a7da03263fb8a1c1d4324d
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-04-25 19:20:15 -0500

    updates

--- mock_test.py
+++ mock_test.py
@@ -3,17 +3,39 @@ from selenium import webdriver
 options = webdriver.ChromeOptions()
 options.binary_location = '/usr/bin/google-chrome-unstable'
 options.add_argument('headless')
+options.add_argument("user-data-dir=/home/jerry/.config/google-chrome-unstable/Default")
 
 driver = webdriver.Chrome(options=options)
 
-driver.get('https://facebook.com/')
+driver.get('https://www.linkedin.com/feed/')
 
 driver.implicitly_wait(10)
 
-email = driver.find_element_by_css_selector('input[type=email]')
-password = driver.find_element_by_css_selector('input[type=password]')
-login = driver.find_element_by_css_selector('input[value="Log In"]')
+##sign_in_buttons = driver.find_elements_by_css_selector('a[class=nav__button-secondary]')
+##for button in sign_in_buttons:
+#    button.click()
+# password = driver.find_element_by_css_selector('input[type=password]')
+# login = driver.find_element_by_css_selector('input[value="Log In"]')
 
-email.send_keys('jjvigil@ucdavis.edu')
+# email.send_keys('jjvigil@ucdavis.edu')
 
-driver.get_screenshot_as_file('main-page.png')
+driver.implicitly_wait(4)
+
+#id = driver.find_element_by_id('username')
+#password = driver.find_element_by_id('password')
+#sign_in_button = driver.find_element_by_css_selector('button[type=submit]')
+
+#id.send_keys('jerry.vigil@mktneutral.com')
+#password.send_keys('XXXXXXX')
+#sign_in_button.click()
+
+driver.implicitly_wait(4)
+
+driver.get_screenshot_as_file('linkedin.png')
+
+##pin_input = driver.find_element_by_css_selector('#input__phone_verification_pin')
+# two_step_submit = driver.find_element_by_css_selector('#two-step-submit-button')
+
+# import code
+# code.interact(local=locals())
+
