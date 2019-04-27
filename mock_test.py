25cf9615abec7815c0637b44a496a277a1e0b6c6
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-04-26 23:02:38 -0500

    updates

--- mock_test.py
+++ mock_test.py
@@ -1,8 +1,10 @@
+import random
 import time
 
 from selenium import webdriver
 from selenium.webdriver.firefox import options
 
+SLEEP_BASE = 180
 DEFAULT_WAIT = 8
 
 linkedin_routes_map = {
@@ -14,11 +16,7 @@ linkedin_routes_map = {
     'jobs-search': ['https://www.linkedin.com/jobs/search/', 'linkedin-jobs-search.png'],
 }
 
-
-def main():
-    profile = webdriver.FirefoxProfile('/home/jerry/.mozilla/firefox/huqgok2n.default')
-    opts = options.Options()
-    opts.headless = True
+def run_routes(profile, opts):
     driver = webdriver.Firefox(profile, options=opts)
     driver.implicitly_wait(DEFAULT_WAIT)
 
@@ -28,6 +26,18 @@ def main():
         driver.get_screenshot_as_file(linkedin_routes_map[route][1])
 
     driver.quit()
+
+def main():
+    random.seed()
+    profile = webdriver.FirefoxProfile('/home/jerry/.mozilla/firefox/huqgok2n.default')
+    opts = options.Options()
+    opts.headless = True
+
+    # run indefinitely
+    while True:
+        run_routes(profile, opts)
+        sleep_range = random.randint(-60, 60)
+        time.sleep(SLEEP_BASE + sleep_range)
 
 if __name__ == '__main__':
     main()
