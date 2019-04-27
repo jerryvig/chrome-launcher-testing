73417a1976716bf784a1b049886686945f175bfa
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-04-26 21:36:59 -0500

    Updates.

--- mock_test.py
+++ mock_test.py
@@ -5,25 +5,28 @@ from selenium.webdriver.firefox import options
 
 DEFAULT_WAIT = 8
 
-profile = webdriver.FirefoxProfile('/home/jerry/.mozilla/firefox/huqgok2n.default')
-options = options.Options()
-# options.headless = True
-driver = webdriver.Firefox(profile, options=options)
-driver.implicitly_wait(DEFAULT_WAIT)
+def main():
+    profile = webdriver.FirefoxProfile('/home/jerry/.mozilla/firefox/huqgok2n.default')
+    opts = options.Options()
+    opts.headless = True
+    driver = webdriver.Firefox(profile, options=opts)
+    driver.implicitly_wait(DEFAULT_WAIT)
 
-linkedin_routes_map = {
-    'feed': ['https://www.linkedin.com/feed/', 'linkedin-feed.png'],
-    'mynetwork': ['https://www.linkedin.com/mynetwork/', 'linkedin-mynetwork.png'],
-    'jobs': ['https://www.linkedin.com/jobs/', 'linkedin-jobs.png'],
-    'notifications': ['https://www.linkedin.com/notifications/', 'linkedin-notifications.png'],
-    'premium': ['https://www.linkedin.com/premium/my-premium/', 'linkedin-premium.png']
-}
+    linkedin_routes_map = {
+        'feed': ['https://www.linkedin.com/feed/', 'linkedin-feed.png'],
+        'mynetwork': ['https://www.linkedin.com/mynetwork/', 'linkedin-mynetwork.png'],
+        'jobs': ['https://www.linkedin.com/jobs/', 'linkedin-jobs.png'],
+        'notifications': ['https://www.linkedin.com/notifications/', 'linkedin-notifications.png'],
+        'premium': ['https://www.linkedin.com/premium/my-premium/', 'linkedin-premium.png'],
+        'jobs-search': ['https://www.linkedin.com/jobs/search/', 'linkedin-jobs-search.png'],
+    }
 
-for route in linkedin_routes_map:
-    driver.get(linkedin_routes_map[route][0])
-    time.sleep(DEFAULT_WAIT)
-    driver.get_screenshot_as_file(linkedin_routes_map[route][1])
+    for route in linkedin_routes_map:
+        driver.get(linkedin_routes_map[route][0])
+        time.sleep(DEFAULT_WAIT)
+        driver.get_screenshot_as_file(linkedin_routes_map[route][1])
 
+    driver.quit()
 
-
-driver.quit()
+if __name__ == '__main__':
+    main()
